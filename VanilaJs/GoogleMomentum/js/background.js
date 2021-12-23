11590ff747013e133ddf1a6a86403be335164d7e
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2021-12-23 15:43:31 +0900

    랜덤 배경화면 추가

---
+++ VanilaJs/GoogleMomentum/js/background.js
@@ -0,0 +1,7 @@
+const images = ["background1.jpg", "background2.jpg", "background3.jpg"];
+
+const randomeimages = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+bgImage.src = `img/${randomeimages}`;
+
+document.body.appendChild(bgImage);

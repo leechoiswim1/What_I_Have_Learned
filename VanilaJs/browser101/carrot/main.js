3dde014fdea3e9bda77ea5993274e3b85ad5817d
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-01-01 00:36:32 +0900

    랜덤 게임 자바스크립트 추가
    - 당근 / 벌레 랜덤 위치생성
    - 클릭 이벤트 추가
    - 시간 셋인터벌 추가

--- VanilaJs/browser101/carrot/main.js
+++ VanilaJs/browser101/carrot/main.js
@@ -1,40 +1,60 @@
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+const background = document.querySelector(".body-background");
+const headerPlayBtn = document.querySelector(".header-playbtn");
+const headerItemTime = document.querySelector(".header-timer__time");
 
-const numBalls = 100;
-const balls = [];
+const resultBox = document.querySelector(".result-box");
+const resultBoxBtn = document.querySelector(".result-box");
+let time = 9;
 
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-
-  balls.push(ball);
-  document.body.append(ball);
+function handleClickCarrot(event) {
+  event.target.style.display = "none";
 }
 
-// Keyframes
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12,
-  };
-
-  let anim = el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` },
-    ],
-    {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out",
+function start() {
+  if (background.children.length < 20) {
+    resultBox.style.display = "none";
+    for (let i = 0; i < 10; i++) {
+      let carrot = document.createElement("img");
+      let bug = document.createElement("img");
+      carrot.classList.add("body-carrot__image");
+      carrot.src = "img/carrot.png";
+      carrot.style.position = "absolute";
+      carrot.style.left = `${Math.floor(Math.random() * 100)}%`;
+      carrot.style.top = `${Math.floor(Math.random() * 100)}%`;
+      carrot.addEventListener("click", handleClickCarrot);
+      bug.classList.add("body-bug__image");
+      bug.src = "img/bug.png";
+      bug.style.position = "absolute";
+      bug.style.left = `${Math.floor(Math.random() * 100)}%`;
+      bug.style.top = `${Math.floor(Math.random() * 100)}%`;
+      background.append(bug, carrot);
     }
-  );
-});
+  } else return;
+}
+
+function setClock() {
+  if (time >= 1) {
+    headerItemTime.innerText = `0:${time}`;
+    return (time = time - 1);
+  } else {
+    headerItemTime.innerText = `0:0`;
+    resultBox.style.display = "flex";
+  }
+}
+
+function handleGameStart() {
+  start();
+  resultBox.style.display = "none";
+  if (time >= 0) {
+    setInterval(setClock, 1000);
+  } else return (time = 9);
+}
+
+function handleGameRestart() {
+  background.innerHTML = "";
+  resultBox.style.display = "none";
+  start;
+}
+
+headerPlayBtn.addEventListener("click", handleGameStart);
+resultBoxBtn.addEventListener("click", handleGameRestart);

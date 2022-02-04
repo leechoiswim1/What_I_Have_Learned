af3e5f093286d888e7ec15593f5830064d614830
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-04 23:45:04 +0900

    6_literal_types_Const.ts

---
+++ TypeScript/TSBigin/6_literal_types_Const.js
@@ -0,0 +1,24 @@
+//Literal Types
+//newName에는 kim만 들어갈수있다.
+var newName;
+var itsMe;
+function newFunc(a) {
+    console.log(a);
+}
+//파라미터 hello만 가능
+newFunc("hello");
+//return값으로 1|0 만 가능
+function newFunc2(a) {
+    return 1;
+}
+function rockPaperScissors(a) {
+    return ["rock"];
+}
+//const변수의 한계 -> 업그레이드 버전: 리터럴타입 (자료를 여러개 담을수있는 const변수)
+//as const 를 쓴다
+//1) obj value값을 그대로 타입으로 지정
+//2) obj 속성들에 모두 readonly 붙여줌
+var documents = { name: "kim" };
+// let documents: { name: "kim" } = { name: "kim" };
+function myFunc3(a) { }
+myFunc3(documents.name);
---
+++ TypeScript/TSBigin/6_literal_types_Const.ts
@@ -0,0 +1,32 @@
+//Literal Types
+//newName에는 kim만 들어갈수있다.
+let newName: "kim";
+
+let itsMe: "solo" | "beauty";
+
+function newFunc(a: "hello") {
+  console.log(a);
+}
+//파라미터 hello만 가능
+newFunc("hello");
+
+//return값으로 1|0 만 가능
+function newFunc2(a: "hello"): 1 | 0 {
+  return 1;
+}
+
+function rockPaperScissors(
+  a: "rock" | "paper" | "scissors"
+): ("rock" | "paper" | "scissors")[] {
+  return ["rock"];
+}
+
+//const변수의 한계 -> 업그레이드 버전: 리터럴타입 (자료를 여러개 담을수있는 const변수)
+//as const 를 쓴다
+//1) obj value값을 그대로 타입으로 지정
+//2) obj 속성들에 모두 readonly 붙여줌
+let documents = { name: "kim" } as const;
+// let documents: { name: "kim" } = { name: "kim" };
+
+function myFunc3(a: "kim") {}
+myFunc3(documents.name);

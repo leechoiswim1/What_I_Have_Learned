c28446bd2f42190c9f4a09ddeadf8e9a627cf5f4
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-06 02:24:19 +0900

    Add react에 ts 적용 및 redux, redux toolkit 적용

--- TypeScript/ts-app/src/App.tsx
+++ TypeScript/ts-app/src/App.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, incrementByAmount, RootState } from "./index";
+import { Dispatch } from "redux";
+
+let box: JSX.IntrinsicElements["div"] = <div>this is box</div>;
+// function App() {
+//   const data = useSelector((state: RootState) => state);
+//   const dispatch: Dispatch = useDispatch();
+//   let [user, setUser] = useState("kim");
+//   // let [user, setUser] = useState<string | number>("kim");
+//   return (
+//     <div>
+//       {data.count}
+//       <button
+//         onClick={() => {
+//           dispatch({ type: "증가" });
+//         }}
+//       >
+//         증가
+//       </button>
+//       <button
+//         onClick={() => {
+//           dispatch({ type: "감소" });
+//         }}
+//       >
+//         감소
+//       </button>
+//       {/* {box}
+//       <Profile name="철수" age="20"></Profile>
+//       <h4>안녕하십니까</h4> */}
+//     </div>
+//   );
+// }
 
 function App() {
-  return <div></div>;
+  const data = useSelector((state: RootState) => state);
+  const dispatch: Dispatch = useDispatch();
+  const countRef = useRef<HTMLInputElement>(null);
+
+  const handleCount = () => {
+    if (countRef.current) {
+      dispatch(incrementByAmount(parseInt(countRef.current.value)));
+      countRef.current.value = "";
+    }
+  };
+
+  return (
+    <div>
+      {data.counter1.count}
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        증가
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        감소
+      </button>
+
+      <input ref={countRef} type="number"></input>
+      <button onClick={handleCount}></button>
+    </div>
+  );
 }
 
 export default App;
--- TypeScript/ts-app/src/index.tsx
+++ TypeScript/ts-app/src/index.tsx
@@ -3,9 +3,70 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+//기본 리덕스 방법
+// import { Provider } from "react-redux";
+// import { createStore } from "redux";
+
+// interface Counter {
+//   count: number;
+// }
+
+// const initialState: Counter = { count: 0 };
+// // const initialState: { count: number } = { count: 0 };
+
+// function reducer(state = initialState, action: { type: string }): Counter {
+//   if (action.type === "증가") {
+//     return { count: state.count + 1 };
+//   } else if (action.type === "감소") {
+//     return { count: state.count - 1 };
+//   } else {
+//     return initialState;
+//   }
+// }
+
+// const store = createStore(reducer);
+
+// // store의 타입 미리 export 해두기
+// export type RootState = ReturnType<typeof store.getState>;
+
+import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+const 초기값 = { count: 0, user: "kim" };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 초기값,
+  reducers: {
+    increment(state) {
+      state.count += 1;
+    },
+    decrement(state) {
+      state.count -= 1;
+    },
+    incrementByAmount(state, action: PayloadAction<number>) {
+      state.count += action.payload;
+    },
+  },
+});
+
+let store = configureStore({
+  reducer: {
+    counter1: counterSlice.reducer,
+  },
+});
+
+//state 타입을 export 해두는건데 나중에 쓸 데가 있음
+export type RootState = ReturnType<typeof store.getState>;
+
+//수정방법 만든거 export
+export let { increment, decrement, incrementByAmount } = counterSlice.actions;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );

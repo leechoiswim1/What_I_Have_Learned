4f7176fe2926cf5653ce089e804469e582568b5b
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-06 04:52:32 +0900

    Add 21_implement

---
+++ TypeScript/TSBigin/21_implement.js
@@ -0,0 +1,19 @@
+//implements keyword
+// class Car_ {
+//     model : string;
+//     price : number = 1000;
+//     constructor(a :string){
+//       this.model = a
+//     }
+//   }
+//   let 붕붕이 = new Car_('morning');
+// class가 interface에 있는 속성을 다 들고있는지 implement로 확인 가능
+class Car_ {
+    constructor(a) {
+        this.price = 1000;
+        this.model = a;
+    }
+}
+let 붕붕이 = new Car_("morning");
+//implements는 타입지정문법이 아님.
+//그래서 따로 타입 지정을 해줘야한다.

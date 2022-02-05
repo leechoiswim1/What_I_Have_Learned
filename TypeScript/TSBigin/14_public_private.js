d50c9bfa93543e54127b221d3d6075ea4cfda677
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-05 23:34:56 +0900

    Add 14_public_private

---
+++ TypeScript/TSBigin/14_public_private.js
@@ -0,0 +1,20 @@
+//public : 모든 자식들이 사용 가능
+//private: 자식들이 수정 불가, 중요한 정보, class안에서만 사용가능
+class User {
+    constructor(a) {
+        this.familyName = "kim";
+        this.name = this.familyName + a;
+    }
+    changeName() {
+        this.familyName = "park";
+    }
+}
+let user1 = new User("jenny");
+console.log(user1.name);
+class Person1 {
+    constructor(name) {
+        this.name = name;
+    }
+}
+let child = new Person1("kim");
+console.log(child);

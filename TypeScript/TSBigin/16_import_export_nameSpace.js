d03279245a135bedbf6b33b1e0ff4c4ef52e180d
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-02-05 23:35:18 +0900

    Add 16_import_export_nameSpace.

---
+++ TypeScript/TSBigin/16_import_export_nameSpace.js
@@ -0,0 +1,3 @@
+// 과거 import방법
+///<reference path="./a.ts"/>
+// NameSpace.name
---
+++ TypeScript/TSBigin/16_import_export_nameSpace.ts
@@ -0,0 +1,8 @@
+// 과거 export 방법
+namespace NameSpace {
+  export type Name = string | number;
+}
+
+// 과거 import방법
+///<reference path="./a.ts"/>
+// NameSpace.name
